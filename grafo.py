7d1be6afbf963621f9d3a91a6c794d3c6b645d3c
RooD7 <rodrigosoualves7@gmail.com>
2018-09-26 11:25:29 -0300

    finish

--- grafo.py
+++ grafo.py
@@ -5,39 +5,34 @@ class grafo(object):
 	
 	TAM = 20
 	def __init__(self):
+		pass
+
+	def create(self):
 		self.v = [0] * self.TAM
 		for i in range(self.TAM):
 			self.v[i] = [0] * self.TAM
-
-		char = ord('A')
-		for i in range(self.TAM):
-			self.v[i].append(chr(i))
-			char += 1
-
-	def create(self):
 		return self.v
 
-	def insertA(self, v1, v2):
+
+	def insertA(self, g, v1, v2):
 		""" Insere arestas no grafo """
-		pass
+		g[v1][v2] += 1
+		g[v2][v1] += 1
 
-	def searchA(self, v1, v2):
+	def searchA(self, g, v1, v2):
 		""" Pesquisa aresta (verifica ligacao) """
-		pass
+		if (g[v1][v2] > 0):
+			return True
 
-	def removeA(self, v1, v2):
+	def removeA(self, g, v1, v2):
 		""" Remove aresta """
-		pass
+		g[v1][v2] = 0
+		g[v2][v1] = 0
 
-	def printV(self, v):
+	def printV(self, g, v):
 		""" Exibir Vertices """
-		pass
+		print(g[v])
 
 	def printG(self, grafo):
 		""" imprime o grafo """
-		st = ""
-		for i in grafo:
-			st += i+" -> "
-			for j in i:
-				st += j+" "
-		print(st)
+		print(grafo)
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class main(object):
 	gf = grafo.grafo()
 
 	while(1):
-		print("1. Criar grafo")
+		print("\n1. Criar grafo")
 		print("2. Inserir aresta")
 		print("3. Pesquisar aresta")
 		print("4. Remover aresta")
@@ -20,24 +20,24 @@ class main(object):
 		if (op == 1):
 			g = gf.create()
 		elif (op == 2):
-			v1 = input("Digite o vertice de origem:")
-			v2 = input("Digite o vertice de destino:")
-			gf.insertA(v1,v2)
+			v1 = int(input("Digite o vertice de origem:"))
+			v2 = int(input("Digite o vertice de destino:"))
+			gf.insertA(g,v1,v2)
 		elif (op == 3):
-			v1 = input("Digite o vertice de origem:")
-			v2 = input("Digite o vertice de destino:")
-			if(gf.searchA(v1,v2)):
-				print("A aresta ("+v1+","+v2+") existe.")
+			v1 = int(input("Digite o vertice de origem:"))
+			v2 = int(input("Digite o vertice de destino:"))
+			if(gf.searchA(g,v1,v2)):
+				print("A aresta ("+str(v1)+","+str(v2)+") existe.")
 			else:
-				print("A aresta ("+v1+","+v2+") nao existe.")
+				print("A aresta ("+str(v1)+","+str(v2)+") nao existe.")
 		elif (op == 4):
-			v1 = input("Digite o vertice de origem:")
-			v2 = input("Digite o vertice de destino:")
-			gf.removeA(v1,v2)
-			print("A aresta ("+v1+","+v2+") foi removida.")
+			v1 = int(input("Digite o vertice de origem:"))
+			v2 = int(input("Digite o vertice de destino:"))
+			gf.removeA(g,v1,v2)
+			print("A aresta ("+str(v1)+","+str(v2)+") foi removida.")
 		elif (op == 5):
-			v1 = input("Digite o vertice:")
-			gf.printV(v1)
+			v1 = int(input("Digite o vertice:"))
+			gf.printV(g,v1)
 		elif (op == 6):
 			gf.printG(g)
 		elif (op == 0):
